4d26ef3af3f1c8f178f3c5ef328aeb0e3db9ad33
Ashish <bansalgashi083@gmail.com>
2024-11-18 14:02:18 +0530

    initial routing setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
+import Body from './components/Body';
 
 function App() {
   return (
-    <div className="text-blue-500">
-      hello world
-    </div>
+    <Body/>
   );
 }
 
