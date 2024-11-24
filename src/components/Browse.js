1224de905a83ce6f1a7e149afa262adbc2de0773
Ashish <bansalgashi083@gmail.com>
2024-11-24 22:48:38 +0530

    signout functionality done by firebase api signout

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import Header from "./Header";
+import { useDispatch } from "react-redux";
+import { removeUser } from "../utils/UserSlice";
+import { auth } from "../utils/Firebase";
+import { signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 const Browse = () => {
+  const navigate = useNavigate();
+  const handleSignout = () => {    
+    signOut(auth)
+      .then(() => {
+        navigate('/')        
+      })
+      .catch((error) => {
+        navigate('/error')
+      });
+  };
+  const dispatch = useDispatch();
   return (
-    <div>Browse</div>
-  )
-}
+    <div>
+      <Header />
+      <button
+        className="font-bold text-green text-5xl w-full cursor-pointer mt-44"
+        onClick={handleSignout}
+      >
+        signout
+      </button>
+    </div>
+  );
+};
 
-export default Browse
+export default Browse;
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='absolute bg-gradient-to-b from-black px-40 py-6 z-10'>
+    <div className='absolute bg-gradient-to-b from-black px-40 py-6 z-10 w-screen'>
         <img 
         className='w-40 '
         src='https://images.ctfassets.net/y2ske730sjqp/821Wg4N9hJD8vs5FBcCGg/9eaf66123397cc61be14e40174123c40/Vector__3_.svg?w=460'
