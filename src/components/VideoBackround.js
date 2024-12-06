cfccd05e9341c2dc6bfca6f984c1c013d21c2de5
Ashish <bansalgashi083@gmail.com>
2024-12-06 21:30:42 +0530

    fething the movie trailer and made it dynamic

--- src/components/VideoBackround.js
+++ src/components/VideoBackround.js
@@ -1,22 +1,32 @@
-import React, { useEffect } from 'react'
-import { FETCH_OPTIONS } from '../utils/Constants';
+import React, { useEffect } from "react";
+import { FETCH_OPTIONS } from "../utils/Constants";
 
-const VideoBackround = ({movieId}) => {
-
-  const getMovievideos=async()=>{
-    const data = await fetch('https://api.themoviedb.org/3/movie/1241982/videos?language=en-US',FETCH_OPTIONS);
+const VideoBackround = ({ movieId }) => {
+  const getMovievideos = async () => {
+    const data = await fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`,
+      FETCH_OPTIONS
+    );
     const json = await data.json();
-    const filteredData = json.results.filter(video=>video.type==="Trailer")
-    const Trailer = filteredData[0];
-  }
-  useEffect(()=>{
+    const filteredData = json.results.filter(
+      (video) => video.type === "Trailer"
+    );
+    const Trailer = filteredData.length ? filteredData[0] : json.results[0];
+    console.log(Trailer);
+  };
+  useEffect(() => {
     getMovievideos();
-  },[])
+  }, []);
   return (
     <div>
-        image
+      <iframe
+        src="https://www.youtube.com/embed/JdSl4RMNtGE?si=wo4LlKD15U6oeUQ2"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+      ></iframe>
     </div>
-  )
-}
+  );
+};
 
-export default VideoBackround
+export default VideoBackround;
