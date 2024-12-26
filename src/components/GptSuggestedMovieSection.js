d899efddb8714a89a574fec79a16e86dad9d6449
Ashish <bansalgashi083@gmail.com>
2024-12-26 22:38:59 +0530

    the movie result data used in the movielist component

--- src/components/GptSuggestedMovieSection.js
+++ src/components/GptSuggestedMovieSection.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import Movielist from "./Movielist";
 
 const GptSuggestedMovieSection = () => {
+  const { movieResult, searchMovieName } = useSelector((store) => store.gpt);
+
+  if(!movieResult) return null;
+
   return (
-    <div>GptSuggestedMovieSection</div>
-  )
-}
+    <div className="bg-black p-4 m-2">
+      <div>
+        {searchMovieName?.map((movie, index) => (
+          <Movielist key={movie} title={movie} movie={movieResult[index]} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default GptSuggestedMovieSection;
--- src/components/Moviecard.js
+++ src/components/Moviecard.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { IMG_CDN_URL } from '../utils/Constants';
 
 const Moviecard = ({title,poster_url}) => {
+  if(!poster_url) return null;
   return (
     <div className='w-52 '>
         <img src={IMG_CDN_URL+poster_url} alt='poster-image' className='aspect-square'></img>      
